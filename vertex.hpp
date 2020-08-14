149d2c42ab6d9b548f694fbccfcb889cab2fe445
abhikjain360 <abhikjain360@gmail.com>
2020-08-14 17:12:13 +0530

    vertex id

--- vertex.hpp
+++ vertex.hpp
@@ -5,11 +5,7 @@
 #include <string>
 #include <type_traits>
 
-// used internally, inside vertex to point to other vertices
-struct joiner {
-    size_t dest;
-    float weight;
-};
+#include "edge.hpp"
 
 // aim: to make sure vertex_list needs no direct access to
 // 		the member variables of the vertex class
@@ -18,44 +14,62 @@ struct joiner {
 class vertex {
 public:
     /* Constructors */
-	vertex()
-        : _Deg(0),
-          _Cap(2),
-          _Wgt(1),
-          _Edges(std::make_unique<joiner[]>(_Cap)),
-          _del_Deg(0),
-          _del_Cap(_Cap),
-          _del_Cache(std::make_unique<size_t[]>(_Cap)) {}
+    vertex(const size_t id) noexcept
+        : _id(id),
+          _deg(0),
+          _cap(2),
+          _wgt(1),
+          _E(std::make_unique<joiner[]>(_cap)),
+          _d_deg(0),
+          _d_cap(_cap),
+          _D(std::make_unique<size_t[]>(_cap)) {}
 
-    vertex(float weight, size_t capacity = 2)
-        : _Deg(0),
-          _Cap(capacity > 1 ? capacity : 2),
-          _Wgt(weight),
-          _Edges(std::make_unique<joiner[]>(_Cap)),
-          _del_Deg(0),
-          _del_Cap(_Cap),
-          _del_Cache(std::make_unique<size_t[]>(_Cap)) {}
+    vertex(const size_t id, const float weight,
+           const size_t capacity = 2) noexcept
+        : _id(id),
+          _deg(0),
+          _cap(capacity > 1 ? capacity : 2),
+          _wgt(weight),
+          _E(std::make_unique<joiner[]>(_cap)),
+          _d_deg(0),
+          _d_cap(_cap),
+          _D(std::make_unique<size_t[]>(_cap)) {}
 
-    vertex(std::unique_ptr<joiner[]> joiner_list, size_t size,
-           float weight = 1)
-        : _Deg(size),
-          _Cap(size > 1 ? size : 2),
-          _Wgt(weight),
-          _Edges(std::move(joiner_list)),
-		  _del_Deg(0),
-          _del_Cap(_Cap),
-          _del_Cache(std::make_unique<size_t[]>(_Cap)) {}
+    vertex(const size_t id, std::unique_ptr<joiner[]> joiner_list,
+           const size_t size, const float weight = 1)
+        : _id(id),
+          _deg(size),
+          _cap(size > 1 ? size : 2),
+          _wgt(weight),
+          _E(std::move(joiner_list)),
+          _d_deg(0),
+          _d_cap(_cap),
+          _D(std::make_unique<size_t[]>(_cap)) {}
 
     /* Setters */
-    void set_weight(float weight) noexcept { this->_Wgt = weight; }
+    void set_weight(const float weight) noexcept { this->_wgt = weight; }
+    void set_id(const size_t id) noexcept { this->_id = id; }
 
     /* Getters */
-    float weight() const noexcept { return _Wgt; }
-    size_t capacity() const noexcept { return _Cap; }
-    size_t degree() const noexcept { return _Deg; }
+    size_t id() const noexcept { return _id; }
+    float weight() const noexcept { return _wgt; }
+    size_t capacity() const noexcept { return _cap; }
+    size_t degree() const noexcept { return _deg; }
+
+    std::optional<edge> edge_at(const size_t dest) const noexcept {
+        if (dest > _deg) return {};
+
+        for (size_t i = 0; i < _cap; ++i) {
+            if (_E[i].dest == dest) {
+                return edge{_E[i].weight, _id, dest};
+            }
+        }
+
+        return {};
+    }
 
     /* Add edges */
-    void join(size_t dest, float weight = 1) {
+    void join(const size_t dest, const float weight = 1) {
         // if no cache left, means degree = last index filled + 1
         //                                = index to be filled now
 
@@ -65,52 +79,52 @@ public:
         // do this if capacity id not enough
         // note that degree == capacity only if
         // no del_cache is left
-        if (_Deg == _Cap) {
+        if (_deg == _cap) {
             size_t lt = 2;
-            while (lt <= _Cap) lt <<= 1;
+            while (lt <= _cap) lt <<= 1;
             this->resize(lt);
 
-            _Edges[_Deg++] = {dest, weight};
+            _E[_deg++] = {dest, weight};
             return;
         }
 
         // do this if we have some cache
         // degree < capacity if cache left
-        if (_del_Deg > 0) {
-            _Edges[_del_Cache[--_del_Deg]] = {dest, weight};
-            ++_Deg;
+        if (_d_deg > 0) {
+            _E[_D[--_d_deg]] = {dest, weight};
+            ++_deg;
             return;
         }
 
         // capacity enough, no cache
-        _Edges[_Deg++] = {dest, weight};
+        _E[_deg++] = {dest, weight};
     }
 
     /* Remove edges :
        returns false if no such vertex existed,
            else returns true if it does and is deleted
     */
-    bool unjoin(size_t dest) {
+    bool unjoin(const size_t dest) {
         // even if last element deleted and added to del cache,
         // no problem
 
         // increas del cache x2 if not enough
-        if (_del_Deg == _del_Cap) {
+        if (_d_deg == _d_cap) {
             size_t lt = 2;
-            while (lt < _del_Deg) lt <<= 1;
+            while (lt < _d_deg) lt <<= 1;
 
-            this->resize_del_cache(lt);
-            _del_Cap = lt;
+            this->resize_d(lt);
+            _d_cap = lt;
         }
 
         // TODO: handle for cases when cache already there;
         // 		 1. indexing if _Edges would not be same as _Deg
         // 		 2. optimize if deleting from last
-        for (size_t i = 0; i < _Cap; ++i) {
-            if (_Edges[i].dest == dest) {
-                _Edges[i]              = {0, 0};
-                _del_Cache[_del_Deg++] = i;
-                --_Deg;
+        for (size_t i = 0; i < _cap; ++i) {
+            if (_E[i].dest == dest) {
+                _E[i]        = {0, 0};
+                _D[_d_deg++] = i;
+                --_deg;
                 return true;
             }
         }
@@ -124,55 +138,53 @@ public:
 
 private:
     // the very basics needed
-    size_t _Deg, _Cap;
-	float _Wgt;
-    std::unique_ptr<joiner[]> _Edges;
+    size_t _id, _deg, _cap;
+    float _wgt;
+    std::unique_ptr<joiner[]> _E;
 
     // del degree will always point at last valid index + 1
     // del cache stores only the 0 based indices
     // of free edges storage
-    size_t _del_Deg = 0, _del_Cap;
-    std::unique_ptr<size_t[]> _del_Cache; // stores indices
+    size_t _d_deg = 0, _d_cap;
+    std::unique_ptr<size_t[]> _D; // stores indices
 
     /* Resize */
     bool resize(size_t size) {
         // resize should not delete existing info
-        if (size <= _Deg) return false;
+        if (size <= _deg) return false;
 
         // reallocation most thread safe
         auto temp = std::make_unique<joiner[]>(size);
 
         if constexpr (std::is_nothrow_move_assignable_v<
                           std::unique_ptr<joiner[]>>) {
-            std::move(_Edges.get(), _Edges.get() + _Cap, temp.get());
+            std::move(_E.get(), _E.get() + _cap, temp.get());
         } else {
-            std::copy(_Edges.get(), _Edges.get() + _Cap, temp.get());
+            std::copy(_E.get(), _E.get() + _cap, temp.get());
         }
 
-        _Edges = std::move(temp);
-        _Cap   = size;
+        _E   = std::move(temp);
+        _cap = size;
 
         return true;
     }
 
-    bool resize_del_cache(size_t size) {
+    bool resize_d(size_t size) {
         // resize should not delete existing info
-        if (size <= _del_Deg) return false;
+        if (size <= _d_deg) return false;
 
         // reallocation most thread safe
         auto del_temp = std::make_unique<size_t[]>(size);
 
         if constexpr (std::is_nothrow_move_assignable_v<
                           std::unique_ptr<size_t[]>>) {
-            std::move(_del_Cache.get(), _del_Cache.get() + _del_Cap,
-                      del_temp.get());
+            std::move(_D.get(), _D.get() + _d_cap, del_temp.get());
         } else {
-            std::copy(_del_Cache.get(), _del_Cache.get() + _del_Cap,
-                      del_temp.get());
+            std::copy(_D.get(), _D.get() + _d_cap, del_temp.get());
         }
 
-        _del_Cache = std::move(del_temp);
-        _del_Cap   = size;
+        _D     = std::move(del_temp);
+        _d_cap = size;
 
         return true;
     }
