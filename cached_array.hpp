02aa908e39b80d95cc12a51223d27fc99f9b53c4
abhikjain360 <abhikjain360@gmail.com>
2020-08-17 09:03:32 +0530

    less code

--- cached_array.hpp
+++ cached_array.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <utility>
+
 #include "resizeable_array.hpp"
 
 template <typename T>
@@ -11,30 +13,26 @@ template <typename T>
 class cached_array {
 public:
     /* Constructors */
-    // UNTESTED
     constexpr cached_array() noexcept : ptr(), d_ptr(){};
-    // UNTESTED
     constexpr cached_array(std::unique_ptr<T[]> p, const size_t size) noexcept
-        : ptr(p, size), d_ptr(size){};
+        : ptr(std::move(p), size), d_ptr(size){};
 
     /* Getters */
-    // UNTESTED
     constexpr size_t degree() const noexcept { return ptr.degree(); }
-    // UNTESTED
     constexpr size_t capacity() const noexcept { return ptr.capacity(); }
 
     /* editting the array values */
-    // UNTESTED
     void add(const T& t) {
         if (d_ptr.degree() > 0)
-            ptr.put(d_ptr.pop(), t);
+            ptr.put(d_ptr.pop().value(), t);
         else
             ptr.put(ptr.degree(), t);
     }
 
-    // UNTESTED
-    template <typename Compare>
-    bool remove(const T& t, Compare compare) {
+    template <typename Compare = bool(const T&, const T&)>
+    void remove(
+        const T& t,
+        Compare compare = [](const T& a, const T& b) -> bool { return a == b; }) {
         for (size_t i = 0; i < ptr.capacity(); ++i) {
             if (compare(ptr[i], t)) {
                 ptr.remove(i);
@@ -44,7 +42,6 @@ public:
     }
 
     /* for assignments */
-    // UNTESTED
     cached_array<T>& operator=(cached_array<T>&& arr) {
         ptr   = std::move(arr.ptr);
         d_ptr = std::move(arr.d_ptr);
@@ -52,7 +49,6 @@ public:
         return *this;
     }
 
-    // UNTESTED
     cached_array<T>& operator=(const cached_array<T>& arr) {
         ptr   = arr.ptr;
         d_ptr = arr.d_ptr;
