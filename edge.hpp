e29c410de65769e4df0909d65297234ef3b683eb
abhikjain360 <abhikjain360@gmail.com>
2020-08-17 21:12:42 +0530

    constructors + explicit

--- edge.hpp
+++ edge.hpp
@@ -4,12 +4,16 @@
 
 // used internally, inside vertex to point to other vertices
 struct joiner {
-	size_t dest;
+    size_t dest;
     float weight;
+
+	joiner() : dest(0), weight(0) {}
+    joiner(size_t d, float w = 1) : dest(d), weight(w) {}
+    explicit joiner(size_t d) : dest(d), weight(1) {}
 };
 
 // to be used by the user, and in graph class
 struct edge {
-	float weight;
-	size_t src, dest;
+    float weight;
+    size_t src, dest;
 };
