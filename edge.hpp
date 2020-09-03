f36256b99cc9a0e1676ef880f13a6e65e991a6c8
abhikjain360 <abhikjain360@gmail.com>
2020-09-03 22:26:08 +0530

    comparision ops

--- edge.hpp
+++ edge.hpp
@@ -10,4 +10,10 @@ struct edge {
     constexpr edge(const size_t s, const size_t d, const float w = 1) noexcept
         : weight(w), src(s), dest(d) {}
     constexpr edge(const int t) noexcept : weight(t), src(t), dest(t) {}
+
+    bool operator<(const edge& e) const { return dest < e.dest; }
+    bool operator<=(const edge& e) const { return dest <= e.dest; }
+    bool operator>=(const edge& e) const { return dest >= e.dest; }
+    bool operator>(const edge& e) const { return dest > e.dest; }
+    bool operator==(const edge& e) const { return dest == e.dest; }
 };
