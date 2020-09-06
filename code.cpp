964a717d8d80029a7f87740b4a41377a0f16afd4
Varad Deouskar <varadd@gmail.com>
2020-09-06 21:03:57 +0530

    added edge to vertex

--- code.cpp
+++ code.cpp
@@ -11,8 +11,12 @@ int main() {
     }
     V[9].set_id(10);
     V[9].join(1);
-
     graph G(std::move(V), 10);
+    auto E = std::make_unique<edge[]>(10);
+    for(size_t i=0;i<7;i++)
+    {
+        E[i] = {i,2,1} ;
+    }
 
     return 0;
 }
