626ec220c21bd81c718681e84ef6e10e3631995c
abhikjain360 <abhikjain360@gmail.com>
2020-09-13 18:01:30 +0530

    ops, tested

--- graph.hpp
+++ graph.hpp
@@ -10,15 +10,13 @@
 class graph {
 public:
     /* Constructors */
-    // UNTESED
     graph(const size_t deg) : V(deg), E(), v_loc(deg + 1) {
         for (size_t i = 1; i <= deg; ++i) {
-            V[i].set_id(i);
+            V.add(i);
             v_loc.add(i, i);
         }
     }
 
-    // UNTESED
     graph(std::unique_ptr<vertex[]> p, const size_t size)
         : V(std::move(p), size), E(size), v_loc(size + 1) {
         for (size_t i = 0; i < size; ++i)
@@ -29,24 +27,26 @@ public:
             }
     }
 
-    // UNTESED
     graph(std::unique_ptr<edge[]> p, const size_t size) : E(std::move(p), size) {
-        size_t mad_id = 0;
+        size_t max_id = 0;
         for (size_t i = 0; i < size; i++) {
-            if (E[i].src > mad_id) {
-                mad_id = E[i].src;
+            if (E[i].src > max_id) {
+                max_id = E[i].src;
                 V.add(vertex(E[i].src));
                 v_loc.add(E[i].src, V.degree() - 1);
             }
-            if (E[i].dest > mad_id) {
-                mad_id = E[i].dest;
+            if (E[i].dest > max_id) {
+                max_id = E[i].dest;
                 V.add(vertex(E[i].dest));
                 v_loc.add(E[i].dest, V.degree() - 1);
             }
-            V[E[i].src].join(E[i]);
-            V[E[i].dest].join(E[i]);
+            V[v_loc[E[i].src]].join(E[i]);
+            V[v_loc[E[i].dest]].join(E[i]);
         }
     }
+
+    /* Copy Constructor */
+    graph(const graph& G) : V(G.V), E(G.E), v_loc(G.v_loc) {}
 
     /* Getters */
     constexpr size_t edge_degree() const noexcept { return E.degree(); }
@@ -57,7 +57,10 @@ public:
         return V[vertex_id].degree();
     }
 
-    // UNTESED
+    constexpr const vertex& operator[](const size_t vertex_id) const {
+        return V[v_loc[vertex_id]];
+    }
+
     template <typename Compare = bool(const edge&, const edge&)>
     void sort_edges(Compare compare = [](const edge& e1, const edge& e2) -> bool {
         return e1.src > e2.src;
@@ -66,32 +69,75 @@ public:
     }
 
     /* adding new elements */
-    void join(const vertex& v) {
-        V.add(v);
-        size_t i = 0;
-        while (i >= 0) E.add(v.next(i));
-    }
-
     void join(const edge& e) {
         E.add(e);
         V[v_loc[e.src]].join(e);
         V[v_loc[e.dest]].join(e);
     }
 
+    void join(const vertex& v) {
+        // if vertex already existing in V is added assertion fails
+        // for those cases add edges of the vertex using other `join` overload
+        assert(v.id() > V.degree());
+        V.add(v);
+        size_t i = 0;
+        while (i < v.capacity()) {
+            E.add(v.next(i));
+        }
+    }
+
+    void unjoin(const edge& e) {
+        E.remove(e);
+        V[v_loc[e.src]].unjoin(e);
+        V[v_loc[e.dest]].unjoin(e);
+    }
+
+    void unjoin(const size_t vertex_id) {
+        V.remove(vertex_id, [](const vertex& v1, const vertex& v2) -> bool {
+            return v1.id() == v2.id();
+        });
+
+        for (size_t i = 0; i < E.capacity(); ++i)
+            if (E[i].src == vertex_id || E[i].dest == vertex_id) E.remove(i);
+    }
+
+    void unjoin(const vertex& v) { this->unjoin(v.id()); }
+
+    void operator+=(const edge& e) { this->join(e); }
+    void operator+=(const vertex& v) { this->join(v); }
+    void operator-=(const edge& e) { this->unjoin(e); }
+    void operator-=(const size_t vertex_id) { this->unjoin(vertex_id); }
+    void operator-=(const vertex& v) { this->unjoin(v); }
+
 private:
     cached_array<vertex> V;
     cached_array<edge> E;
     index_keeper v_loc;
+
+    bool directed = true;
 };
 
-// UNTESED
-graph& operator+(graph& G, const edge& e) {
+graph operator+(graph G, const edge& e) {
     G.join(e);
     return G;
 }
 
-// UNTESED
-graph& operator+(graph& G, const vertex& v) {
+graph operator+(graph G, const vertex& v) {
     G.join(v);
+    return G;
+}
+
+graph operator-(graph G, const edge& e) {
+    G.unjoin(e);
+    return G;
+}
+
+graph operator-(graph G, const vertex& v) {
+    G.unjoin(v);
+    return G;
+}
+
+graph operator-(graph G, const size_t vertex_id) {
+    G.unjoin(vertex_id);
     return G;
 }
