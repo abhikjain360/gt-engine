343006dd8e82045eefb1771a00f18569664a6fe4
Varad Deouskar <varadd@gmail.com>
2020-10-20 22:37:54 +0530

    Added Kruskal's Algo

--- code.cpp
+++ code.cpp
@@ -1,38 +1,25 @@
+#include<vector>
+#include<iostream>
 #include <cstdlib>
-#include <iostream>
-
 #include "graph.hpp"
+#include "kruskal.hpp" 
+using namespace std;
 
-int
-main()
+int main()
 {
-    // graph constructor 1
-    graph g1(10);
-
-    // graph constructor 2
-    std::unique_ptr<vertex[]> p = std::make_unique<vertex[]>(10);
-    for (int i = 0; i < 10; ++i)
-        p[i].set_id(i + 1);
-    graph g2(std::move(p), 10);
-
-    // graph constructor 3
-    std::unique_ptr<edge[]> e = std::make_unique<edge[]>(20);
-    for (size_t i = 0; i < 20; ++i)
-        e[i] = { static_cast<size_t>(rand() % 10 + 1),
-                 static_cast<size_t>(rand() % 10 + 1),
-                 static_cast<float>(rand() % 100 + 1) };
-    graph g3(std::move(e), 20);
-    g3.sort_edges();
-
-    // joining edges
-    g1.join({ 1, 2, 1 });
-    g1.join({ 2, 3, 1 });
-    g1.join({ 3, 4, 1 });
-    g1.join({ 4, 5, 1 });
-
-    // unjoining edges
-    g1.unjoin({ 1, 2, 1 });
-    g1.unjoin({ 2, 3, 1 });
-
-    return 0;
+	graph g1(5);
+	g1.join({1, 2, 1});
+	g1.join({2, 3, 7});
+	g1.join({3, 4, 4});
+	g1.join({4, 1, 9});
+	g1.join({1, 5, 5});
+	g1.join({2, 5, 2});
+	g1.join({3, 5, 7});
+	g1.join({4, 5, 1});
+	g1.sort_edges(compare);
+	graph g2 = KruskalsMinimumSpaningTree(g1) ;
+	for(int i =0;i<g2.edge_degree();i++)
+	{
+		cout<<g2.E[i].src<<" "<<g2.E[i].dest<<" "<<g2.E[i].weight<<endl ;
+	}
 }
---
+++ kruskal.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+
+#include<vector>
+#include<iostream>
+#include <cstdlib>
+#include "graph.hpp"
+#define fast ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
+
+using namespace std;
+
+bool compare(const edge &e1, const edge &e2) { return e1.weight < e2.weight; }
+void make_set(int v,vector<int> &parent)
+{
+	parent[v] = v ;
+}
+int find_set(int v,vector<int> &parent)
+{
+	while(v!=parent[v])
+	{
+		v = parent[v];
+	}
+	return v ;
+}
+bool union_set(int a,int b,vector<int> &parent)
+{
+	a = find_set(a,parent);
+	b = find_set(b,parent);
+	if(a!=b)
+	{
+		parent[b] = a ;
+		return true ;
+	}
+	return false ;
+
+}
+graph KruskalsMinimumSpaningTree(graph &g1)
+{	
+	int n = g1.degree() ;
+	graph mst(n) ;
+	vector<int> parent;
+	parent.resize(n,-1);
+	for(int i = 0;i<n;i++)
+	{
+		make_set(i,parent) ;
+	}
+	for (int i = 0; i < g1.edge_degree(); i++)
+	{
+		if(union_set(g1.E[i].src,g1.E[i].dest,parent))
+		{
+			mst.join({g1.E[i].src,g1.E[i].dest,g1.E[i].weight}) ;
+		}
+	}
+
+
+	return mst;
+}
