7ed873a4d26cb100759d8c5c2b4dc40fc930e5bd
Varad Deouskar <varadd@gmail.com>
2020-10-20 22:54:10 +0530

    Implemented Rank And Path Compression in kruskal

--- kruskal.hpp
+++ kruskal.hpp
@@ -19,12 +19,16 @@ int find_set(int v,vector<int> &parent)
 	}
 	return v ;
 }
-bool union_set(int a,int b,vector<int> &parent)
+bool union_set(int a,int b,vector<int> &parent,vector<int> &depth)
 {
 	a = find_set(a,parent);
 	b = find_set(b,parent);
 	if(a!=b)
 	{
+		if (depth[a]<depth[b])
+		{
+			swap(a,b) ;
+		}
 		parent[b] = a ;
 		return true ;
 	}
@@ -37,13 +41,15 @@ graph KruskalsMinimumSpaningTree(graph &g1)
 	graph mst(n) ;
 	vector<int> parent;
 	parent.resize(n,-1);
+	vector<int> depth;
+	depth.resize(n,1) ;
 	for(int i = 0;i<n;i++)
 	{
 		make_set(i,parent) ;
 	}
 	for (int i = 0; i < g1.edge_degree(); i++)
 	{
-		if(union_set(g1.E[i].src,g1.E[i].dest,parent))
+		if(union_set(g1.E[i].src,g1.E[i].dest,parent,depth))
 		{
 			mst.join({g1.E[i].src,g1.E[i].dest,g1.E[i].weight}) ;
 		}
